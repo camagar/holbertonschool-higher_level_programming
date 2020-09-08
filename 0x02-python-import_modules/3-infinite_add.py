6625202b5f82b00afc584c96253848e781dc81cf
camagar <mauriciogrestrepo@gmail.com>
2020-09-08 00:59:14 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    Nargs = len(sys.argv)
+    suma = 0
+    if Nargs > 1:
+        for i in range (1, Nargs):
+            aux = int(sys.argv[i])
+            suma += aux
+        print("{:d}".format(suma))
+    else:
+        print("{}".format(0))
