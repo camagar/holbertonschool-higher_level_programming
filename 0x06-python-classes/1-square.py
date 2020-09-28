b0825797c384b8d68774dc9142df4f2cc646bfef
camagar <mauriciogrestrepo@gmail.com>
2020-09-28 00:01:47 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class Square:
+    """This class that defines a square"""
+    def __init__(self, size):
+        """size is private"""
+        self.__size = size
