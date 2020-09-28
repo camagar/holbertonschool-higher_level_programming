fd143c724677c3c3f4567af64a7cf23deeb0f9ce
camagar <mauriciogrestrepo@gmail.com>
2020-09-28 01:22:25 +0000

    properties

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class Square:
+    """This class that defines a square"""
+    def __init__(self, size=0):
+        """size is private"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """define area"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
