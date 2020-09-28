1e7f03e5e4cf9af45587a3ef6b2ce73b76c0101b
camagar <mauriciogrestrepo@gmail.com>
2020-09-28 01:37:30 +0000

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class Square:
+    """This class that defines a square"""
+    def __init__(self, size=0):
+        """size is private"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """define area"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """define size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """print the square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
