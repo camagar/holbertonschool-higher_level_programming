d59c1bcf829740f5189e80655027c0b798ca6c28
camagar <mauriciogrestrepo@gmail.com>
2021-01-21 04:52:37 +0000

    lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" counting number of lines in a File"""
+"""create"""
 
 
 def number_of_lines(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        numlines = 0
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            numlines += 1
+    """this function return number of lines"""
 
-    return (numlines)
+    i = 0
+    with open(filename, "r") as my_file:
+        for line in my_file:
+            i += 1
+    my_file.close()
+    return (i)
