589539ebd6446ca82b7b95c4aa85d7061cf5f5aa
camagar <mauriciogrestrepo@gmail.com>
2021-01-21 03:51:12 +0000

    rread file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" read a file """
+"""create"""
 
 
 def read_file(filename=""):
-    with open(filename,  encoding='utf-8') as f:
-        print(f.read(), end="")
+    """read file function"""
+
+    with open(filename, "r") as my_file:
+        print(my_file.read(), end="")
+    my_file.close()
