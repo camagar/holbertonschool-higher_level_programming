7c99db565f3a066ac6fefe19c9fda4d000473d69
camagar <mauriciogrestrepo@gmail.com>
2021-03-25 04:29:38 +0000

    concats s

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+#!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+const line1 = fs.readFileSync(fileA, 'utf8');
+const line2 = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, line1 + line2, 'utf8');
