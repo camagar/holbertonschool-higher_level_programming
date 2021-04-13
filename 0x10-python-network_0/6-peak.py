6efb72b43fcf46451baefdb8c019c775118e4fa9
camagar <mauriciogrestrepo@gmail.com>
2021-04-13 04:57:16 +0000

    Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,15 +3,8 @@
 
 
 def find_peak(list_of_integers):
-		if len(list_of_integers) == 0:
-				return None
-		if list_of_integers[0] >= list_of_integers[1]:
-				return (list_of_integers[0])
-		else:
-				for i in range(1, len(list_of_integers)):
-						try:
-								if (list_of_integers[i] >= list_of_integers[i - 1] and
-									 list_of_integers[i] >= list_of_integers[i + 1]):
-										return list_of_integers[i]
-						except:
-								return list_of_integers[len(list_of_integers)]
+    """Find peak"""
+    if len(list_of_integers) == 0:
+        return None
+    list_of_integers.sort(reverse=True)
+    return list_of_integers[0]
